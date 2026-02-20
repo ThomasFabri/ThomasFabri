f72db4bc49a763b8afe69a41bbf56f0cdd9c0c46
Thomas Fabri <tsfabri@gmail.com>
2026-02-20 19:22:08 +0000

    buildin animation

--- components/About.tsx
+++ components/About.tsx
@@ -1,11 +1,33 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import { Hammer } from "lucide-react";
+import { gsap } from "gsap";
 
 export default function About() {
-  return (
-    <section className="min-h-1000 bg-black/95 text-white">
+  const hammerRef = useRef(null);
 
+  useEffect(() => {
+    gsap.fromTo(
+      hammerRef.current,
+      { rotation: -40 },
+      {
+        rotation: 0,
+        duration: 0.4,
+        ease: "power2.in",
+        yoyo: true,
+        repeat: -1, // infinito
+      }
+    );
+  }, []);
+
+  return (
+    <section className="min-h-200 bg-black/95 text-white pt-60 text-center justify-items-center">
+      <h1>Buildin...</h1>
+
+      <div ref={hammerRef} className="inline-block">
+        <Hammer size={40} />
+      </div>
     </section>
   );
 }
